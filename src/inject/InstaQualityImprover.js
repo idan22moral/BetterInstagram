011a4252566e928e321e5e5acae303f0eb1a2b79
idan22moral <idan22moral@gmail.com>
2018-09-20 20:56:55 +0300

    Added Profile Picture Improver

--- src/inject/InstaQualityImprover.js
+++ src/inject/InstaQualityImprover.js
@@ -4,6 +4,9 @@
 const INSTAGRAM_MEDIA_CLASS = "_2di5p";
 const INSTAGRAM_IMAGE_PARENT_CLASS = "_4rbun";
 const INSTAGRAM_IMAGE_GRANDP_CLASS = "_e3il2 _pmuf1";
+const INSTAGRAM_API_URL_BASE = 'https://i.instagram.com/api/v1/users/';
+const PROFILE_PICTURE_CLASS = '_6q-tv';
+const USERNAME_TITLE_CLASS = 'AC5d8 notranslate';
 
 class InstaQualityImprover {
     constructor() {
@@ -13,8 +16,11 @@ class InstaQualityImprover {
     /**
      * Applies an event handler to the document, that gets triggered when a valid media DOM Element is being inserted.
      */
-    apply()
-    {
+    apply() {
+        // Improve the profile picture
+        this.improveProfilePicture();
+
+        // Improve the future-added pictures
         addEventListener("DOMNodeInserted", (e) => {
             this.improveSelectedMediaQuality();
         });
@@ -44,17 +50,44 @@ class InstaQualityImprover {
         let bestQuailtyImageLink = imgElement.srcset.split(",").reverse()[0].split(" ")[0];
 
         // Change the source of the image to the best quality if necessary
-        if(bestQuailtyImageLink !== imgElement.src)
-                imgElement.src = bestQuailtyImageLink;
+        if (bestQuailtyImageLink !== imgElement.src)
+            imgElement.src = bestQuailtyImageLink;
     }
 
     /**
      * Improve the selected media.
      */
-    improveSelectedMediaQuality()
-    {
+    improveSelectedMediaQuality() {
         for (const media of this.getMediaImageElements()) {
             this.improveImageQuality(media);
         }
+    }
+
+    /**
+     * Improves the quality of the profile picture
+     */
+    improveProfilePicture() {
+        var profileId = this.getId().then(profileId => {
+            fetch(`${INSTAGRAM_API_URL_BASE}${profileId}/info/`).then(res => {
+                res.json().then(jsonObject => {
+                    var hdUrl = jsonObject['user']['hd_profile_pic_url_info']['url'];
+                    document.getElementsByClassName(PROFILE_PICTURE_CLASS)[0].src = hdUrl;
+                    console.log("profile picture improved!");
+                });
+            });
+        })
+
+    }
+
+    /**
+     * Gets the user id of by the username in the profile
+     */
+    getId() {
+        var username = document.getElementsByClassName(USERNAME_TITLE_CLASS)[0].innerText;
+        return fetch(`https://www.instagram.com/${username}/?__a=1`).then(res => {
+            return res.json().then(jsonObject => {
+                return jsonObject["graphql"]["user"]["id"];
+            });
+        });
     }
 }
